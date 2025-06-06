a2bc1cf002a942378ceb31ec28d13e42454db69f
marechalg <guewenmarechal@protonmail.com>
2025-06-06 16:35:37 +0200

    to handle mails and contact

---
+++ public/script/homeButtons.js
@@ -0,0 +1,7 @@
+document.getElementById('about').addEventListener('click', () => {
+    window.open("about.html");
+})
+
+document.getElementById('contact').addEventListener('click', () => {
+    window.open("mailto:guewen.marechal@etudiant.univ-rennes.fr", "_blank");
+})
