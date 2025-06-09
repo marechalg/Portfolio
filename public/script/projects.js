70bb2472481fe75d8ec0aab270ca5d5cc0e01c11
marechalg <guewenmarechal@protonmail.com>
2025-06-09 22:47:39 +0200

    first steps

--- public/script/projects.js
+++ public/script/projects.js
@@ -1,7 +1,12 @@
-const fs = require('node:fs');
 const projects = require('../data/projects.json');
 const { adlog } = require('../../server');
 
 for (const project of projects) {
-    
+    for (const category of project.categories) {
+        document.getElementById(category).innerHTML = `
+            <article>
+                <p>haha</p>
+            </article>
+        `
+    }
 }
