531e768b3b359761cf4282814137ada019c97eb4
marechalg <guewenmarechal@protonmail.com>
2026-01-22 21:23:21 +0100

    Switched to PHP server and restructured files

---
+++ pages/home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Guéwen Maréchal</title>
+    <link rel="icon" href="/assets/icons/likoubra.png">
+
+    <link rel="stylesheet" href="../styles/main.css">
+</head>
+
+<body>
+
+    <?php
+        $currentPage = basename(__FILE__);
+        require_once $_SERVER['DOCUMENT_ROOT'] . '/pages/partials/header.php'
+    ?>
+
+    <main></main>
+
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/pages/partials/footer.php' ?>
+</body>
+
+</html>
---
+++ pages/partials/header.php
@@ -0,0 +1,22 @@
+<header>
+    <nav>
+        <h1>Portfolio</h1>
+        <ul>
+            <?php $class = $currentPage == 'home.php' ? 'here' : ''; ?>
+            <li><a href="/" class="<?php echo $class ?>">Accueil</a></li>
+            <?php $class = $currentPage == 'about.php' ? 'here' : ''; ?>
+            <li><a href="/about" class="<?php echo $class ?>">À Propos</a></li>
+            <?php $class = $currentPage == 'projects.php' ? 'here' : ''; ?>
+            <li><a href="/projects" class="<?php echo $class ?>">Projets</a></li>
+            <?php $class = $currentPage == 'contact.php' ? 'here' : ''; ?>
+            <li><a href="/contact" class="<?php echo $class ?>">Contact</a></li>
+        </ul>
+        <div class="burger">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </nav>
+</header>
