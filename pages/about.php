a0694610e9e25259ca0e009c945fbb24b1a7d24d
marechalg <guewenmarechal@protonmail.com>
2026-01-28 11:24:26 +0100

    Added diploms and formations section

--- pages/about.php
+++ pages/about.php
@@ -54,15 +54,60 @@
 
                 <ul>
                     <li><a href="/assets/cv.pdf" download><button>Télécharger</button></a></li>
-                    <li><button id="view">Consulter</button></li>
+                    <li>
+                        <?php if (preg_match('/(android|iphone|ipad|mobile)/i', $_SERVER['HTTP_USER_AGENT'] ?? '')): ?>
+                            <a href="/assets/cv.pdf" target="_blank"><button>Consulter</button></a>
+                        <?php else: ?>
+                            <button id="view">Consulter</button>
+                        <?php endif; ?>
+                    </li>
                 </ul>
             </article>
         </section>
-
         <dialog id="cv">
             <button autofocus style="position:absolute;opacity:0;pointer-events:none"></button>
             <iframe src="/assets/cv.pdf" frameborder="0" id="pdf"></iframe>
         </dialog>
+
+        <section class="diploms">
+            <h1>Diplômes et Formations</h1>
+
+            <ul>
+                <article>
+                    <div class="time">2024 - Auj.</div>
+                    <h2>BUT Informatique</h2>
+                    <strong>IUT de Lannion</strong>
+                    <strong title="Développement d'applications">Parcours A</strong>
+                    <p>Formation en développement logiciel, bases de données, réseaux et systèmes, sur 3 ans. Spécialisation en optimisation et développement d'applications.</p>
+                    <em>En cours - 2<sup>ème</sup> année</em>
+                </article>
+
+                <article>
+                    <div class="time">2021 - 2024</div>
+                    <h2 title="Sciences Technologiques de l'Industrie et du Développement Durable">BAC STI2D</h2>
+                    <strong>Lycée Polyvalent Chaptal à Saint-Brieuc</strong>
+                    <strong title="Systèmes d'Informations et Numérique">Spécialité SIN</strong>
+                    <p>Formation scientifique avec approfondissement en informatique, éléctronique et conception.</p>
+                    <em>Mention Assez Bien</em>
+                </article>
+
+                <article>
+                    <div class="time">2022 - 2024</div>
+                    <h2>Certificat d'Études Musicales</h2>
+                    <strong>Conservatoire de Saint-Brieuc</strong>
+                    <strong>Euphonium</strong>
+                    <p>Diplôme validant la fin de 3ème cycle de pratique instrumentale.</p>
+                    <em>Mention Très Bien</em>
+                </article>
+
+                <article>
+                    <div class="time">2021</div>
+                    <h2 title="Sciences Technologiques de l'Industrie et du Développement Durable">Premiers Secours Citoyen</h2>
+                    <strong>Collège Jean Racine à Saint-Brieuc</strong>
+                    <p>Formation de premiers secours, dans le cadre de la Classe Défense et Sécurité Globale.</p>
+                </article>
+            </ul>
+        </section>
 
         <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/pages/partials/up.php' ?>
     </main>
